018002a5c92291385e2370a235f139be80f178bc
Dean Lunz <deanlunz@createdbyx.com>
2013-11-17 01:29:33 -0800

    minor edits, updated name spaces in assemblyinfo.cs file

--- ContentManager/Properties/AssemblyInfo.cs
+++ ContentManager/Properties/AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CBX.ContentManager")]
+[assembly: AssemblyTitle("Codefarts.ContentManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CBX.ContentManager")]
+[assembly: AssemblyProduct("Codefarts.ContentManager")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
