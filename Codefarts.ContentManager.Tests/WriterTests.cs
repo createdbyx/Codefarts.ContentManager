fd12a1e5b8bf017f5f0bc8473c9a61949f6990bb
Dean Lunz <deanlunz@createdbyx.com>
2014-06-24 11:12:39 -0700

    Added unit tests for writing

---
+++ Codefarts.ContentManager.Tests/WriterTests.cs
@@ -0,0 +1,56 @@
+﻿// <copyright>
+//   Copyright (c) 2012 Codefarts
+//   All rights reserved.
+//   contact@codefarts.com
+//   http://www.codefarts.com
+// </copyright>
+
+namespace Codefarts.ContentManager.Tests
+{
+    using System.IO;
+    using System.Runtime.InteropServices;
+
+    using Codefarts.ContentManager.Tests.Models;
+    using Codefarts.ContentManager.Tests.Writers;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class WriterTests
+    {
+        private ContentManager<string> contentManager;
+
+        private string tempPath;
+
+        [TestInitialize]
+        public void StartUp()
+        {
+            this.tempPath = Path.GetTempPath();
+            this.contentManager = new ContentManager<string>(this.tempPath);
+            this.contentManager.Register(new TestModelWriter());
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.contentManager.Unload();
+            this.contentManager = null;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var data = new TestModel() { FileName = "TestName.txt", FloatValue = 5.25f, IntegerValue = 123 };
+            try
+            {
+                this.contentManager.Save("testData.xml", data);
+                var file = Path.Combine(this.contentManager.RootDirectory, "testData.xml");
+                Assert.AreEqual(true, File.Exists(file));
+            }
+            catch (System.Exception ex)
+            {
+                Assert.Fail("Should not have thrown excepton!", ex);
+            }
+        }
+    }
+}
---
+++ Codefarts.ContentManager.Tests/Writers/TestModelWriter.cs
@@ -0,0 +1,55 @@
+﻿// <copyright>
+//   Copyright (c) 2012 Codefarts
+//   All rights reserved.
+//   contact@codefarts.com
+//   http://www.codefarts.com
+// </copyright>
+
+namespace Codefarts.ContentManager.Tests.Writers
+{
+    using System;
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    using Codefarts.ContentManager.Tests.Models;
+
+    public class TestModelWriter : IWriter<string>
+    {
+        public Type Type
+        {
+            get
+            {
+                return typeof(TestModel);
+            }
+        }
+
+        public void Write(string key, object data, ContentManager<string> content)
+        {
+            if (this.Type != data.GetType())
+            {
+                throw new InvalidCastException("data is not of the right type.");
+            }
+
+            var model = data as TestModel;
+            var serializer = new XmlSerializer(this.Type);
+            var file = !Path.IsPathRooted(key) ? Path.Combine(content.RootDirectory, key) : key;
+            using (var writer = XmlWriter.Create(file))
+            {
+                serializer.Serialize(writer, model);
+            }
+        }
+
+        public bool CanWrite(string key, ContentManager<string> content)
+        {
+            var file = !Path.IsPathRooted(key) ? Path.Combine(content.RootDirectory, key) : key;
+            var info = new FileInfo(file);
+            return true;
+        }
+
+        public void WriteAsync(string key, object data, ContentManager<string> content, Action completedCallback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
